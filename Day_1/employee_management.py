e8695e011248d2fedc57754894d13e13d33de2b5
chetan2110 <chetanchowdary2002@gmail.com>
2025-09-09 15:52:13 +0530

    9th_sep_2025

---
+++ Day_1/employee_management.py
@@ -0,0 +1,44 @@
+employees = []
+
+employee=('Bhanu',22,50000, True)
+employees.append(employee)
+
+employee=('Mahesh',46,4000.50, True)
+employees.append(employee)
+
+employee=('Vaishnavi',21,40000.75, True)
+employees.append(employee)
+
+print("After add all employees: ",employees)
+
+i=0
+search='Vaishnavi'
+index=-1
+for emp in employees:
+    if emp[0] == search:
+        index=i
+        break
+    i+=1
+
+if index==-1:
+    print("Employee not found")
+
+else:
+    search_employee = employees[index]
+    print(search_employee)
+    salary = float(input('Salary : '))
+    employees[index]=employee
+print('After search and update: ',employees)
+
+employee=('Dravid',50,200.75,True)
+employees.append(employee)
+print('after add Dravid: ',employees)
+#delete last employee
+employees.pop()
+print('after delete Dravid: ',employees)
+
+
+#delete employee ny position
+position = 1
+employees.pop(position)
+print('after delete Mahesh: ',employees)
---
+++ Day_1/find_sum.py
@@ -0,0 +1,7 @@
+def sum(num1,num2):
+    return num1+num2
+
+result = sum(3,5)
+result_1 = sum(2.5,6.3)
+print(result)
+print(result_1)
